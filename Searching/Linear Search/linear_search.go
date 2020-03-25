6adc6da1bd352f426c8f97bb67e6c7860562905b
Aakash Dinkar <35952953+aakashdinkar@users.noreply.github.com>
2020-03-25 15:53:42 +0530

    Create linear_search.go
    iterative Linear Search

---
+++ Searching/Linear Search/linear_search.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func linear_search(arr[] int, n int, num int) int {
+	i := 0
+	for(i<n){
+		if arr[i] == num{
+			return i
+		}
+		i = i + 1
+	}
+	return -1
+}
+
+func main(){
+	var n,num int
+	fmt.Print("Enter number of elements:")
+	fmt.Scan(&n)
+	fmt.Print("Enter the elements in sorted order")
+	arr := make([]int, n)
+	i:=0
+	for(i<n){
+		fmt.Print("Enter element ",i+1," :")
+		fmt.Scan(&arr[i])
+		i = i + 1
+	}
+	fmt.Println("Enter the number to be searched:")
+	fmt.Scan(&num)
+	index := linear_search(arr,n,num)
+	fmt.Print("Element ",num," found at location ",index+1)
+}
