93faf5241f68750262ce1b4983cdd0d3cfdae949
Aakash Dinkar <35952953+aakashdinkar@users.noreply.github.com>
2020-03-25 16:24:15 +0530

    Update linear_search

--- Searching/Linear Search/linear_search.go
+++ Searching/Linear Search/linear_search.go
@@ -17,7 +17,7 @@ func main(){
 	var n,num int
 	fmt.Print("Enter number of elements:")
 	fmt.Scan(&n)
-	fmt.Print("Enter the elements in sorted order")
+	fmt.Println("Enter the elements")
 	arr := make([]int, n)
 	i:=0
 	for(i<n){
@@ -25,7 +25,7 @@ func main(){
 		fmt.Scan(&arr[i])
 		i = i + 1
 	}
-	fmt.Println("Enter the number to be searched:")
+	fmt.Print("Enter the number to be searched:")
 	fmt.Scan(&num)
 	index := linear_search(arr,n,num)
 	fmt.Print("Element ",num," found at location ",index+1)
